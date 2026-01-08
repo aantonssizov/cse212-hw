75335408525d6fcdd0be468714e278bb85fb8faf
Anton Syzov <aantonssizov@gmail.com>
2026-01-08 23:38:58 +0100

    Week 1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Security.AccessControl;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,16 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // Create an array with the length as was provided in the corresponding parameter
+        double[] result = new double[length];
+
+        // Loop through length parameter starting from 0
+        for (int i = 0; i < length; i++)
+            // Add into the array at the current position in loop by multiplying number parameter by current position plus 1
+            result[i] = number * (i + 1);
+
+        // return the final array
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +40,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Get slice of the List from the end of the List
+        List<int> slice = data.GetRange(data.Count - amount, amount);
+
+        // Remove the slice in the original List
+        data.RemoveRange(data.Count - amount, amount);
+
+        // Insert the slice to the start of the List
+        data.InsertRange(0, slice);
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,28 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        int[] result = new int[select.Length];
+
+        for (int selectItm = 0, list1Counter = 0, list2Counter = 0; selectItm < select.Length; selectItm++)
+        {
+            if (select[selectItm] == 1)
+            {
+                result[selectItm] = list1[list1Counter++];
+            }
+            else
+            {
+                result[selectItm] = list2[list2Counter++];
+            }
+        }
+
+        return result;
     }
 }
