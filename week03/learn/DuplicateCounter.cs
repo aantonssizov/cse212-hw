ff4a94e5488a3d13e074f3b88be10e4e548ecedd
Anton Syzov <aantonssizov@gmail.com>
2026-01-22 16:40:52 +0100

    W03 learn

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -24,7 +24,7 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        var hashSet = data.ToHashSet();
+        return data.Length - hashSet.Count;
     }
 }
